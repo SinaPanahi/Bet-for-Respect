94cbc51f8c52029dd17b2c3041a5a51fa52c3f47
sumitparida2002 <80693203+sumitparida2002@users.noreply.github.com>
2023-03-24 17:18:28 -0400

    Home page index working with flask server

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
 @app.route('/')
 @app.route('/home')
 def home():
     # changed home route
+    # hitting home right now but when Sina integrates the index into home should hit home.html
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'About')
-
+    return render_template('about.html', title='About')
